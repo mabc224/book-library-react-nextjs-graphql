65e2ffc4f803f2385513c71c3be849037b232df5
mabc224 <mabc224@gmail.com>
2023-03-27 01:24:18 +0500

    Fix Collection Pages by using reusable component

--- pages/want-to-read/index.js
+++ pages/want-to-read/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { gql, useQuery } from '@apollo/client'
 import DashboardLayout from '@/components/layout/dashboard-layout'
+import BookList from '@/components/book-list'
 
 const GET_USER_BOOKS = gql`
   query userBooks($userId: ID!, $collection: Collection!) {
@@ -29,25 +29,7 @@ export default function WantToRead () {
 
  return (
   <div className="flex flex-row flex-wrap justify-center -mx-3">
-   {userBooks?.length > 0 && userBooks?.map((book) => {
-    return (<div key={book.bookId}
-                 className="flex-shrink max-w-full w-full sm:w-1/3 lg:w-1/4 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-     <div className="flex flex-row sm:block hover-img">
-      <Link href={`/book/edit/${book.bookId}`}>
-       <img className="max-w-full	w-full mx-auto object-contain sm:h-24 h-48" src="/next.svg" alt="book title"/>
-      </Link>
-      <div className="py-0 sm:py-3 pl-3 sm:pl-0">
-       <h3 className="text-lg font-bold leading-tight mb-2">
-        <Link href={`/book/edit/${book.bookId}`}>{book.title}</Link>
-       </h3>
-       <p className="hidden md:block text-gray-600 leading-tight mb-1">{book.author} <span
-        className="hidden md:inline-block text-gray-400 leading-tight mb-1">, {book.date} </span></p>
-       <span
-        className="text-gray-500 inline-block h-3 border-l-2 border-red-600 mr-2"></span>Want To Read
-      </div>
-     </div>
-    </div>)
-   })}
+   <BookList data={userBooks} collectionType={'Want To Read'} />
   </div>
  )
 }
