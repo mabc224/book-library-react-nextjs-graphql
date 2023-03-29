b45ca9f711872ef0261bdc552c8fd2fec15fa888
mabc224 <mabc224@gmail.com>
2023-03-29 14:38:59 +0500

    Add about page

---
+++ pages/about/index.js
@@ -0,0 +1,7 @@
+export default function About () {
+ return (
+  <p className="w-full text-center text-3xl font-semibold leading-loose text-gray-900 dark:text-white">
+   Developed By Arsalan Bilal
+  </p>
+ )
+}
