787571b1bb6302deec553816fff9e69d30358d9f
mabc224 <mabc224@gmail.com>
2023-03-30 01:34:58 +0500

    Add dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-   "./app/**/*.{js,ts,jsx,tsx}",
-   "./pages/**/*.{js,ts,jsx,tsx}",
-   "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+ content: [
+  './public/index.html',
+  './app/**/*.{js,ts,jsx,tsx}',
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+ ],
+ darkMode: 'class',
+ theme: {
+  extend: {},
+ },
+ plugins: [],
 }
